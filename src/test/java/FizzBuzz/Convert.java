9e7edd80be1e23f1cfa90dec66840a9a4065a6dc
GiulioAlberti <g3alberti@gmail.com>
2021-11-17 18:57:54 +0100

    Refactor Test

--- src/test/java/FizzBuzz/Convert.java
+++ src/test/java/FizzBuzz/Convert.java
@@ -8,29 +8,28 @@ import org.junit.jupiter.params.provider.ValueSource;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Convert {
+
+    private final FizzBuzz fizzBuzz = new FizzBuzz();
+
     @ParameterizedTest
     @CsvSource({"1,1", "2,2","41,41"})
     void numbers_not_divisible_by_3_or_5_to_Strings(int number, String expected) {
-        FizzBuzz fizzBuzz= new FizzBuzz();
         assertEquals(expected, fizzBuzz.convert(number));
     }
 
     @ParameterizedTest
     @ValueSource(ints = {3,6,24})
     void multiples_of_3_to_Fizz(int number) {
-        FizzBuzz fizzBuzz= new FizzBuzz();
         assertEquals("Fizz", fizzBuzz.convert(number));
     }
     @ParameterizedTest
     @ValueSource(ints = {5,10,100})
     void multiples_of_5_to_Buzz(int number) {
-        FizzBuzz fizzBuzz= new FizzBuzz();
         assertEquals("Buzz", fizzBuzz.convert(number));
     }
     @ParameterizedTest
     @ValueSource(ints = {15,45,75})
     void multiples_of_15_to_FizzBuzz(int number) {
-        FizzBuzz fizzBuzz= new FizzBuzz();
         assertEquals("FizzBuzz", fizzBuzz.convert(number));
     }
 }
