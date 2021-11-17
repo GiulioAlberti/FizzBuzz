e87aa4bb32c9822911ff4fe713add9569c73eac5
GiulioAlberti <g3alberti@gmail.com>
2021-11-17 20:08:08 +0100

    We print numbers from 1 to 100

--- src/main/java/FizzBuzz/FizzBuzz.java
+++ src/main/java/FizzBuzz/FizzBuzz.java
@@ -1,30 +1,29 @@
 package FizzBuzz;
-public class FizzBuzz{
-    public static void main(String ... args) {
-/*       for (int i=1; i<=105; i++){
-          String res="";
-    if(i%3==0){
-                res+="Fizz";
-            }
-            if (i%5==0){
-                res+="Buzz";
-            }
-            if (i%7==0){
-                res+="Bang";
-            }
-            System.out.println(res.isEmpty()? i: res);
-        }
-     } */
+
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class FizzBuzz {
+
+    public static void main(String... args) {
+
     }
+
     public String convert(int number) {
-        if(number %15==0){
+        if (number % 15 == 0) {
             return "FizzBuzz";
-        }else if(number %5==0){
+        } else if (number % 5 == 0) {
             return "Buzz";
-        }else if(number %3==0){
+        } else if (number % 3 == 0) {
             return "Fizz";
-        }else{
-        return Integer.toString(number);
+        } else {
+            return Integer.toString(number);
         }
+    }
+
+    public void print() {
+        Stream<String> fizzBuzzes = IntStream.range(1, 101).mapToObj(this::convert);
+        System.out.println(fizzBuzzes.collect(Collectors.joining(", ")));
     }
 }
--- src/test/java/FizzBuzz/Convert.java
+++ src/test/java/FizzBuzz/Convert.java
@@ -1,6 +1,5 @@
 package FizzBuzz;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -12,24 +11,26 @@ public class Convert {
     private final FizzBuzz fizzBuzz = new FizzBuzz();
 
     @ParameterizedTest
-    @CsvSource({"1,1", "2,2","41,41"})
-    void numbers_not_divisible_by_3_or_5_to_Strings(int number, String expected) {
+    @CsvSource({"1, 1", "2, 2", "41, 41"})
+    void numbers_not_divisible_by_3_or_5_to_strings(int number, String expected) {
         assertEquals(expected, fizzBuzz.convert(number));
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {3,6,24})
+    @ValueSource(ints = {3, 6, 24})
     void multiples_of_3_to_Fizz(int number) {
         assertEquals("Fizz", fizzBuzz.convert(number));
     }
+
     @ParameterizedTest
-    @ValueSource(ints = {5,10,100})
+    @ValueSource(ints = {5, 10, 100})
     void multiples_of_5_to_Buzz(int number) {
         assertEquals("Buzz", fizzBuzz.convert(number));
     }
+
     @ParameterizedTest
-    @ValueSource(ints = {15,45,75})
-    void multiples_of_15_to_FizzBuzz(int number) {
+    @ValueSource(ints = {15, 45, 75})
+    void multiples_of_3_and_5_to_FizzBuzz(int number) {
         assertEquals("FizzBuzz", fizzBuzz.convert(number));
     }
 }
