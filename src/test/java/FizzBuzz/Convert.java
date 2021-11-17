0b118f9e5cf40e0fb30a4d3ffc1351489bdfa69d
GiulioAlberti <g3alberti@gmail.com>
2021-11-17 18:35:06 +0100

    Removed duplications

--- src/test/java/FizzBuzz/Convert.java
+++ src/test/java/FizzBuzz/Convert.java
@@ -25,4 +25,9 @@ public class Convert {
         FizzBuzz fizzBuzz= new FizzBuzz();
         assertEquals("Fizz", fizzBuzz.convert(6));
     }
+    @Test
+    void number_24_to_Fizz() {
+        FizzBuzz fizzBuzz= new FizzBuzz();
+        assertEquals("Fizz", fizzBuzz.convert(24));
+    }
 }
